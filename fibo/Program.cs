2717fa35cc1b2b0c965438a827319a0218d44929
PRDG-k <lasrb6605@naver.com>
2025-04-08 11:58:01 +0900

    피보나치 꼬리 재귀 최적화

---
+++ fibo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Fibonacci(int n, int a = 0, int b = 1)
+{
+    if (n == 0)
+    {
+        return a;
+    }
+
+    if (n == 1)
+    {
+        return b;
+    }
+
+    return Fibonacci(n - 1, b, a+b);
+}
+
+int n = 10;
+int result = Fibonacci(n);
+Console.Write(result);
